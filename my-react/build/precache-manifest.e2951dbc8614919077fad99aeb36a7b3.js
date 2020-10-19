ceb84db38c6f65e8781383c02ef559f8653143ab
salmane <salmane-ben@live.com>
2020-10-19 01:18:35 +0100

    Deploying

---
+++ my-react/build/precache-manifest.e2951dbc8614919077fad99aeb36a7b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ef0711165e7b6b74d282c382adeb8e",
+    "url": "/To-Do/my-react/index.html"
+  },
+  {
+    "revision": "5f39f03b5d4c1fac1a14",
+    "url": "/To-Do/my-react/static/css/main.9d5462a2.chunk.css"
+  },
+  {
+    "revision": "7f2dbb4b7e650f4f8130",
+    "url": "/To-Do/my-react/static/js/2.41ba0cda.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/To-Do/my-react/static/js/2.41ba0cda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f39f03b5d4c1fac1a14",
+    "url": "/To-Do/my-react/static/js/main.7e78ffba.chunk.js"
+  },
+  {
+    "revision": "dd2aad360bf6173f2ac2",
+    "url": "/To-Do/my-react/static/js/runtime-main.e78ee462.js"
+  }
+]);
